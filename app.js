25a87b473e3486c92287946142b03e20ac4faba8
Isuru sathsara <isurusathsara183@gmail.com>
2021-01-10 03:15:11 +0530

    commented

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//require dependencies to work in the server
 const express=require("express");
 const jwt=require("jsonwebtoken");
 const bodyParser=require('body-parser');
@@ -13,32 +14,31 @@ const cookieParser=require("cookie-parser");
 const {requireAuth, requireAuth1} = require('./middleware/authmiddleware');
 const fileupload=require('express-fileupload');
 app.use(fileupload());
-var profileImage;
-dotenv.config({path : './.env'})
+var profileImage; //define variable to save the path of the images uploaded
+dotenv.config({path : './.env'}) //retrive the .env file
 
-const storage = multer.diskStorage({
+const storage = multer.diskStorage({//multer helps in uploading images through forms in html to store them in a folder in server
     destination:'./public/uploads',
     filename: function(req, file, cb){
         cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
-        console.log(file);
     }
 });
 
-const upload=multer({
+const upload=multer({//storing images in folder
     storage:storage
 }).single('uploaded_image');
 
-app.use(express.json());
+app.use(express.json()); ///using the dependencies
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(upload); 
 app.use(cookieParser());
 
-app.engine('html',require('ejs').renderFile, cons.swig);
-app.set('view engine', 'html');
+app.engine('html',require('ejs').renderFile, cons.swig); //remove cross origin error faults
+app.set('view engine', 'html');//define the view engine as html
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public')); //static path defined
 
-const PORT=3000;
+const PORT=3000; //port that the application runs in
 const connection=mysql.createConnection({
     //  host:'mysql.metropolia.fi',
     //  user:'jekaters',
@@ -48,6 +48,7 @@ const connection=mysql.createConnection({
        user:'root',
        password:'17/Eng/099',
        database:'blogdata'
+       //mysql database connection is established
 });
 connection.connect(function(error){
     if(error){
@@ -57,7 +58,7 @@ connection.connect(function(error){
         console.log('connected');
     }
 });
-app.use('/auth',express.static(__dirname +'auth'));
+//routing the application pages to view pages according to the url
 app.get('/',function(req,res){
     connection.query('SELECT * FROM post ',async (error,results)=>{
 
@@ -72,7 +73,9 @@ app.get('/register',function(req,res){
         message:"damn"
     })
 })
+
 app.get('/post_edit',requireAuth, (req,res)=>{
+    //routes the post edtting data to the html page to render it
     const id=req.query.id;
     const Title=req.query.Title;
     const Published=req.query.Published;
@@ -86,7 +89,7 @@ app.get('/post_edit',requireAuth, (req,res)=>{
    res.render('post_edit',{qs: qs});
 })
 app.get('/home',requireAuth1, (req,res)=>{
-
+//home page retrived here
     connection.query('SELECT * FROM folowers WHERE UserID=?',[res.locals.user.id],async (error,results)=>{
         if(error){
             console.log(error);
@@ -98,20 +101,20 @@ app.get('/home',requireAuth1, (req,res)=>{
     });
    
 });
-app.post('/reg', (req,res)=>{
-   res.send(req.body);
-});
+
 app.post('/auth/register', (req,res)=>{
+    //register form validation and user registration api 
     const { fname, mname, lname, email, password, passwordconfirm, mobile, intro} = req.body;
     console.log(req.body);
+    //check if email already exists
     connection.query('SELECT email FROM user WHERE email=?',[email],async (error,results)=>{
         if(error){
             console.log(error);
         }
-        var img_name=profileImage;
+        var img_name=profileImage; //image name saved in the files
 
                         if(results.length>0){
-                         return res.json({success:"That email already exists"});
+                         return res.json({success:"That email already exists"}); //send json responces to the request
                         }
                         else if(password !== passwordconfirm){
                           return res.json({success:"passwords doesn't match"});
@@ -125,25 +128,24 @@ app.post('/auth/register', (req,res)=>{
                                console.log(error);
                            }
                            else{
+                               //user entered to the database without any errors
                            return res.json({success:"You have successfully registered. Please login to your account"});
                            }
                        })                   
     });
 })
-
+//the post request of editing the user details 
 app.post('/auth/edit',requireAuth,function async(req,res){
     const { fname, mname, lname, email, mobile, intro , currentpassword} = req.body;
     id=res.locals.user.id;
     connection.query('SELECT * FROM user WHERE id = ?',[id], async (error, results)=>{
     if(!(await bcrypt.compare(currentpassword, results[0].password))){
-        res.status(401).render('edit',{
-            message: 'Current password entered is incorrect',
-        })
+        return res.json({success:'Current password entered is incorrect'})//check if password is correct
     }
-    else{
-    if(fname){
+    else{//if password is correct database details are updated
+    if(fname){//check the input is given
         connection.query('UPDATE user SET firstname=? WHERE id=?',[fname, id],async (error,results)=>{
-            if(error){
+            if(error){//first name updated
                 console.log(error);
             }
             else{
@@ -153,9 +155,8 @@ app.post('/auth/edit',requireAuth,function async(req,res){
         });
     }
     if(lname){
-        console.log("req.body");
         connection.query('UPDATE user SET lastname=? WHERE id=?',[lname, id],async (error,results)=>{
-            if(error){
+            if(error){//lastname updated
                 console.log(error);
             }
             else{
@@ -167,7 +168,7 @@ app.post('/auth/edit',requireAuth,function async(req,res){
     if(mname){
         console.log("req.body");
         connection.query('UPDATE user SET middlename=? WHERE id=?',[mname, id],async (error,results)=>{
-            if(error){
+            if(error){//update the middle name
                 console.log(error);
             }
             else{
@@ -179,7 +180,7 @@ app.post('/auth/edit',requireAuth,function async(req,res){
     if(email || !(email==res.locals.user.email)){
         console.log("req.body");
         connection.query('UPDATE user SET email=? WHERE id=?',[email, id],async (error,results)=>{
-            if(error){
+            if(error){//update the email
                 console.log(error);
             }
             else{
@@ -190,7 +191,7 @@ app.post('/auth/edit',requireAuth,function async(req,res){
     if(mobile){
         console.log("req.body");
         connection.query('UPDATE user SET mobile=? WHERE id=?',[mobile, id],async (error,results)=>{
-            if(error){
+            if(error){//update the mobile
                 console.log(error);
             }
             else{
@@ -202,7 +203,7 @@ app.post('/auth/edit',requireAuth,function async(req,res){
     if(intro){
         console.log("req.body");
         connection.query('UPDATE user SET intro=? WHERE id=?',[intro, id],async (error,results)=>{
-            if(error){
+            if(error){//update the intro
                 console.log(error);
             }
             else{
@@ -211,44 +212,47 @@ app.post('/auth/edit',requireAuth,function async(req,res){
             }
         });
     }
-    return res.json({success:"Edited"});
+    return res.json({success:"Edited"});//json responce
 }
 });
 });
-
+//image is uploaded to a folder using the post request 
 app.post('/imageupload',function async(req,res){
     var file = req.files.uploaded_image;
     console.log(file);
     if(file.mimetype == "image/jpeg" ||file.mimetype == "image/png"||file.mimetype == "image/gif" ){
-                 profileImage ='uploads'+Date.now()+file.name;
+                 profileImage ='uploads'+Date.now()+file.name;//save the file name 
                  console.log(profileImage);          
-        file.mv('public/'+profileImage,async function(err){
+        file.mv('public/'+profileImage,async function(err){//move the file to public folder
             if (err)
             console.log(err);
         });
     }
 });
+//login post request
 app.post('/auth/login',function async(req,res){
     try{
-        const {email, password, uploaded_image} = req.body;
+        const {email, password, uploaded_image} = req.body; //json body retrived
         connection.query('SELECT * FROM user WHERE email = ?',[email], async (error, results)=>{
             if(results.length==0 || !(await bcrypt.compare(password, results[0].password))){
-               return res.json({success:"Email or Password is incorrect"});
+               return res.json({success:"Email or Password is incorrect"}); //passwords didint match as in database
             }
             else{
                 const id =results[0].id;
                 const token = jwt.sign({id: id}, process.env.JWT_SECRET,{
-                    expiresIn:process.env.JWT_EXPIRES_IN
+                    expiresIn:process.env.JWT_EXPIRES_IN //create a json web token to keep track of the user
+                    //token expiring time given
                 });
               
                 const cookieOptions = {
                     expires: new Date(
-                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 
+                        Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 //store the token in a cookie
+                        //the time of expiring of the cookie 
                     ),
                     httpOnly: true
                 }
-                res.cookie('jwt',token, cookieOptions);
-                res.json({success:"Sucessfully logged in"});
+                res.cookie('jwt',token, cookieOptions); //cookie created
+                res.json({success:"Sucessfully logged in"}); //json responce 
             
             }
         })
@@ -257,19 +261,22 @@ app.post('/auth/login',function async(req,res){
     }
 })
 
-app.post('/logout',function(req,res){
+app.post('/logout',function(req,res){ //logout route
     res.cookie('jwt','',{maxAge:1});
    return res.json({success:"logout successfull"});
 })
-app.get('/edit', requireAuth ,(req,res)=>{
+//get request routing
+app.get('/edit', requireAuth ,(req,res)=>{ 
     res.render('edit',{message:"Leave any detail as it is if changes are not required"});
 });
 app.get('/profile', requireAuth, (req,res)=>{
     res.render('profile',{message:false});
 });
+//editing the previous posts 
 app.post('/edit_post', requireAuth, (req,res)=>{
     const {id}=req.body;
-    connection.query('SELECT * FROM post WHERE id=?',[id],async (error,results)=>{
+    //database searched to find the relavant post and pass it ti the postedit.html as json responds
+    connection.query('SELECT * FROM post WHERE id=?',[id],async (error,results)=>{ 
         if(error){
             return res.json({success:"Error"});
         }
@@ -279,7 +286,7 @@ app.post('/edit_post', requireAuth, (req,res)=>{
     });
 });
 
-
+//postview  request to render the html page
 app.get('/postview', requireAuth ,(req,res)=>{
     connection.query('SELECT * FROM post WHERE id=?',[req.query.post],async (error,results)=>{
         if(error){
@@ -288,17 +295,17 @@ app.get('/postview', requireAuth ,(req,res)=>{
         else{
            
             connection.query('SELECT * FROM user WHERE id=?',[results[0].userID],async (error,results1)=>{
-                if(error){
+                if(error){//user details 
                     console.log(error);
                 }
                 else{
                     connection.query('SELECT * FROM post_comment WHERE postID=?',[req.query.post],async (error,results2)=>{
-                        if(error){
+                        if(error){//comments the user posted
                             console.log(error);
                         }
                         else{
                             connection.query('SELECT * FROM folowers WHERE postID=?',[req.query.post],async (error,result)=>{
-                          
+                              //liked posts by the user
                                 res.render('postview',{qs: results[0], author: results1[0],comment:results2,count:result, message:false});
                             });
                             
@@ -310,7 +317,7 @@ app.get('/postview', requireAuth ,(req,res)=>{
         }
     });
 });
-
+//deleting the comments post request
 app.post('/auth/deletecomment', requireAuth, (req,res)=>{
     const {post,publish}=req.body;
     console.log(publish);
@@ -324,7 +331,7 @@ app.post('/auth/deletecomment', requireAuth, (req,res)=>{
         }
       });
 })
-
+//commenting to posts
 app.post('/auth/comment', requireAuth, (req,res)=>{
     
     const {id, name, content} = req.body;
@@ -343,6 +350,8 @@ app.post('/auth/comment', requireAuth, (req,res)=>{
         })
     }
 });
+
+//liking the posts
 app.post('/auth/like', requireAuth, (req,res)=>{
   const {id}=req.body;
     connection.query('INSERT INTO folowers SET ?',{userID: res.locals.user.id, postID: id},(error,results)=>{
@@ -355,7 +364,7 @@ app.post('/auth/like', requireAuth, (req,res)=>{
         }
     })
 });
-
+//unliking the post
 app.post('/auth/unlike', requireAuth, (req,res)=>{
     const {id}=req.body;
     connection.query('DELETE from folowers WHERE postID=? AND UserID=?',[id, res.locals.user.id],async (error,results)=>{
@@ -367,6 +376,7 @@ app.post('/auth/unlike', requireAuth, (req,res)=>{
         }
     })
 });
+//search page rendering
 app.get('/search',requireAuth,(req,res)=>{
     const category=req.query.category;
     connection.query('SELECT * FROM post WHERE category=? AND userID!=?',[category , res.locals.user.id],async (error,results)=>{
@@ -385,6 +395,7 @@ app.get('/search',requireAuth,(req,res)=>{
         }
     });
 })
+//search validation
 app.post('/search', requireAuth, (req,res)=>{
 
     const {category}=req.body;
@@ -395,7 +406,7 @@ app.post('/search', requireAuth, (req,res)=>{
         return res.json({success:"search"});
     }
 });
-
+//edit post details and validate the form inputs
 app.post('/auth/editpost', requireAuth, (req,res)=>{
     const {id, title, summary, content, category, uploaded_image} = req.body;
   
@@ -448,7 +459,7 @@ connection.query('UPDATE post SET updatedAt=? WHERE id=?',[date_ob, id],async (e
 return res.json({success:"Succesfully edited"});
 
 });
-
+//upload a post to the blog 
 app.post('/auth/upload', requireAuth, (req,res)=>{
     const { title, content, summary, category} = req.body;
     if(category =="select")
@@ -470,7 +481,7 @@ app.post('/auth/upload', requireAuth, (req,res)=>{
     
     }
 });
-
+//delete a post
 app.post('/auth/delete', requireAuth, (req,res)=>{
     const {id} = req.body;
     connection.query('DELETE from post WHERE id=?',[id],async (error,results)=>{
@@ -483,10 +494,11 @@ app.post('/auth/delete', requireAuth, (req,res)=>{
       });
 });
 
-
+//default page route
 app.get('*',function(req,res){
     res.send('404 page not found');
 })
+//run server
 app.listen(PORT,function(){
     console.log(`Server is listening to ${PORT}`);
 })
--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -1,4 +1,4 @@
-const jwt=require("jsonwebtoken");
+const jwt=require("jsonwebtoken"); //validates the user using the json web token
 const mysql = require("mysql");
 const db= mysql.createConnection({
     // host:'mysql.metropolia.fi',
@@ -12,19 +12,19 @@ const db= mysql.createConnection({
 
 })
 const requireAuth = (req,res,next)=>{
-    const token = req.cookies.jwt;
+    const token = req.cookies.jwt; //retrive the token from the cookie
 
     if(token){
-        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken)=>{
+        jwt.verify(token, process.env.JWT_SECRET, (err, decodedToken)=>{  //verify whether the token is the same
             if(err){
                 console.log(err.message);
                 res.redirect('login',{
-                    message: 'Please login',
+                    message: 'Please login', //if the token is damaged or tampered redirect to login
                 })
             }else{
                 console.log(decodedToken);
                 /**********/
-                try{
+                try{  //get the user details from database
                     db.query('SELECT * FROM user WHERE id = ?',[decodedToken.id], async (error, results)=>{
                         if(!results){
                             res.status(401).render('login',{
@@ -32,12 +32,12 @@ const requireAuth = (req,res,next)=>{
                             })
                         }
                         else{                         
-                           res.locals.user =results[0];
+                           res.locals.user =results[0];//save the details in locals.user variable
                            db.query('SELECT * FROM post WHERE userID = ?',[decodedToken.id], async (error, results1)=>{
                             if(!results1){
                                    console.log(error);
                             }else{
-                                res.locals.posts=results1;
+                                res.locals.posts=results1; //save the details in locals.posts variable
                                 
                                 next();
                             }
@@ -55,11 +55,11 @@ const requireAuth = (req,res,next)=>{
     }
     else{
         res.render('login',{
-            message:"Please login",
+            message:"Please login", //if token mismatch or if token not found redirect to login
         })
     }
 }
-const requireAuth1 = (req,res,next)=>{
+const requireAuth1 = (req,res,next)=>{ //similar to the previous requireauth function but retrives post data as well
     const token = req.cookies.jwt;
 
     if(token){
